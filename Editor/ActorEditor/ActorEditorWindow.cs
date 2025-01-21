3b6174b8fe25310859897970ed41e1b030b3e4be
Fingbel <fingbel@gmail.com>
2025-01-21 21:45:12 +0100

    removed duplicate method

--- Editor/ActorEditor/ActorEditorWindow.cs
+++ Editor/ActorEditor/ActorEditorWindow.cs
@@ -149,28 +149,6 @@ public partial class ActorEditorWindow : EditorWindow
         }
         
     }
-
-    private void RefreshPortraitPreview()
-    {   
-        if(portraitAnimation == "Fixed") {portraitAnimationDelta = 0;Repaint();return;}
-        switch(portraitAnimation)
-        {
-            case "Talk":portraitAnimationDelta = 0; break;
-            case "Nod":portraitAnimationDelta = 10; break;
-            case "Shake":portraitAnimationDelta = 20; break;
-        }
-        portraitAnimationTick += Time.deltaTime;
-        if(portraitAnimationTick >= 3f)
-        {
-            
-            currentPortraitFrame++;
-            if(currentPortraitFrame >= portraitAnimationDelta+10)
-                currentPortraitFrame = portraitAnimationDelta;            
-            portraitAnimationTick = 0;
-            Repaint();
-        }
-        
-    }
     
     private void LoadSpriteSheets()
     {
